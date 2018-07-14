4514cdbed9c21b17c68d4970bcc3e6620118488d
Michael Comella <michael.l.comella@gmail.com>
2018-07-14 12:02:39 -0700

    Add initial manifest, tsconfig, publish scripts.

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+rm -rf dist/
+mkdir dist/
+
+tsc && \
+    tsc -p src/options && \
+    tsc -p spec && jasmine && \
+    zip -r -FS dist/bundle.xpi \
+        src/dist/ \
+        LICENSE \
+        README.md \
+        manifest.json
