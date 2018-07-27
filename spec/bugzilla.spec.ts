a7180f173b983034f856211d166ac05bf6a79485
Michael Comella <michael.l.comella@gmail.com>
2018-07-27 08:21:44 -0700

    Issue #1: Add Bugzilla URL operations.

---
+++ spec/bugzilla.spec.ts
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+describe('The Bugzilla namespace', () => {
+
+    it('can identify bugzilla urls', () => {
+        [
+            'https://bugzilla.mozilla.org',
+            'http://bugzilla.mozilla.org',
+            'https://bugzilla.mozilla.org/show_bug.cgi?id=1471868#c0'
+        ].forEach(url => {
+            expect(Bugzilla.isBzURL(url)).toBeTruthy(url);
+        });
+    });
+
+    it('can identify non-bugzilla urls', () => {
+        [
+            'https://mozilla.org',
+            'https://bugzilla.org',
+            'https://fsf.org',
+        ].forEach(url => {
+            expect(Bugzilla.isBzURL(url)).toBeFalsy(url);
+        });
+    });
+
+    it('can extract bug numbers from bugzilla urls', () => {
+        [
+            'https://bugzilla.mozilla.org/show_bug.cgi?id=1471868',
+            'https://bugzilla.mozilla.org/show_bug.cgi?id=1471868#c0',
+        ].forEach(url => {
+            expect(Bugzilla.getBugNumberFromURL(url)).toBe(1471868);
+        });
+    });
+
+    it('gets null when extracting bug numbers from non-bugzilla urls', () => {
+        [
+            'https://bugzilla.mozilla.org/',
+            'https://fsf.org',
+            'https://bugzilla.mozilla.org/buglist.cgi?quicksearch=firefox',
+        ].forEach(url => {
+            expect(Bugzilla.getBugNumberFromURL(url)).toBeFalsy(url);
+        });
+    });
+});
---
+++ src/ts/bugzilla.ts
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+namespace Bugzilla {
+
+    const REGEX_BZ_URL = /^https?:\/\/bugzilla\.mozilla\.org/
+    // sample: https://bugzilla.mozilla.org/show_bug.cgi?id=1471868
+    const REGEX_BZ_URL_BUG_NUMBER = /^https?:\/\/bugzilla\.mozilla\.org\/show_bug\.cgi\?id=([0-9]+)/
+
+    export function isBzURL(url: string): boolean {
+        return REGEX_BZ_URL.test(url);
+    }
+
+    export function getBugNumberFromURL(url: string): number | null {
+        const match = url.match(REGEX_BZ_URL_BUG_NUMBER);
+        if (match && match[1]) {
+            return parseInt(match[1]);
+        }
+
+        return null;
+    }
+}
