1078446a5a2c2dc116e10832ee85cd6fa43faf00
Michael Comella <michael.l.comella@gmail.com>
2018-08-04 07:35:21 -0700

    Issue #10: Add title selector to GithubPageIssue.

--- src/ts/github/page_issue.ts
+++ src/ts/github/page_issue.ts
@@ -7,8 +7,14 @@
  */
 namespace GithubPageIssue {
 
+    export const SELECTOR_ISSUE_TITLE = '.js-issue-title';
+
     // Some intuitive selectors will include non-visible comments like the
     // new comment field or the comment edit box.
     export const SELECTOR_COMMENT_VISIBLE = '.unminimized-comment';
     export const SELECTOR_COMMENT_BODY_VISIBLE = `${SELECTOR_COMMENT_VISIBLE} .comment-body`;
+
+    export function getTitleElement() {
+        return document.querySelector(SELECTOR_ISSUE_TITLE) as HTMLSpanElement;
+    }
 }
