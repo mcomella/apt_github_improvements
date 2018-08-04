ecb45e2daa739b160e85c85dd6f2734f92d0ce18
Michael Comella <michael.l.comella@gmail.com>
2018-08-04 07:35:59 -0700

    Issue #10: Link issue nums in PR titles; no mutation obs.

---
+++ src/ts/features/link_issues_in_pr_titles.ts
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/** In PR titles, makes issue numbers clickable links. */
+namespace FeatureLinkIssuesInPRTitles {
+
+    export function inject() {
+        // todo: mutation + don't do if done; next commit.
+        const titleElement = GithubPageIssue.getTitleElement();
+        linkIssuesInTitle(titleElement);
+    }
+
+    function linkIssuesInTitle(titleElement: HTMLSpanElement) {
+        if (titleElement.querySelector('a')) { // Someone, us?, has already added links.
+            return;
+        }
+
+        const linkedTitleFragment = document.createDocumentFragment();
+        const {ownerName, repoName} = PageDetect.getOwnerAndRepo();
+        titleElement.innerText.split(GithubIssue.REGEX_NUMBER).forEach((splitText, index) => {
+            if (index % 2 === 0) {
+                linkedTitleFragment.appendChild(document.createTextNode(splitText));
+            } else { // Matched issue numbers are odd indices.
+                const link = document.createElement('a');
+                link.text = '#' + splitText;
+                link.href = GithubURLs.issueFromNumber(ownerName, repoName, parseInt(splitText));
+                linkedTitleFragment.appendChild(link);
+            }
+        });
+
+        const titleTextNode = titleElement.firstChild;
+        if (titleTextNode) {
+            titleElement.replaceChild(linkedTitleFragment, titleTextNode);
+        }
+    }
+}
